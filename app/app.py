f1e56505fc0f16a4bba99c07ce1d3fa2a2bd7fcd
Lucas Maurice <lmaurice@progi.com>
2024-09-20 17:00:59 -0400

    Add a demo for PVC with RWMany

--- app/app.py
+++ app/app.py
@@ -6,14 +6,12 @@ from signal import raise_signal, SIGINT
 from socket import gethostname
 from yaml import dump
 from time import time
+from argparse import ArgumentParser
 
 app = Flask(__name__)
 
 # get the host and port from the environment
-HOST = getenv("HOST", "0.0.0.0")
-PORT = int(getenv("PORT", "8080"))
 HOSTNAME = gethostname()
-
 STARTUP_TIME = time()
 
 # Configure the version file
@@ -38,6 +36,17 @@ calls = 0
 ready = True
 alive = True
 
+parser = ArgumentParser(prog='kube-demo-tool', description='A tool to for demonstrate and test some Kubernetes features.')
+parser.add_argument('--host', help='The host to bind the service to.', default=getenv("HOST", "0.0.0.0"))
+parser.add_argument('--port', help='The port to bind the service to.', default=getenv("PORT", "8080"))
+parser.add_argument('--storage', help="Specify a path to enable the storage feature.", default=None)
+
+args = parser.parse_args()
+
+HOST = args.host
+PORT = int(args.port)
+STORAGE_PATH = args.storage
+
 
 # Get the others nodes from SRV records
 def getNodes():
@@ -52,6 +61,21 @@ def getNodes():
             out.append(data.json())
 
     return out
+
+
+def prepareStorage():
+    if STORAGE_PATH is not None:
+        try:
+            with open(STORAGE_PATH, "a") as f:
+                f.write(f"Started at {STARTUP_TIME} with version {VERSION}\n")
+                f.close()
+        except FileNotFoundError:
+            with open(STORAGE_PATH, "w") as f:
+                f.write(f"Started at {STARTUP_TIME} with version {VERSION}\n")
+                f.close()
+        except Exception as e:
+            print(f" * Error while preparing storage: {e}")
+            exit(1)
 
 
 # Health endpoint - ready
@@ -138,12 +162,20 @@ def hello():
         global calls
         calls += 1
 
-    return {
+    ret_data = {
         "hostname": HOSTNAME,
         "calls": calls,
         "version": VERSION,
         "uptime": time() - STARTUP_TIME,
     }
+
+    # Read the last line from the storage file
+    if STORAGE_PATH is not None:
+        with open(STORAGE_PATH, "r") as f:
+            data = f.readlines()
+            ret_data["last_line"] = data[-1].strip()
+
+    return ret_data
 
 
 @app.route("/")
@@ -181,6 +213,42 @@ def config():
     return dump(response), 200, {"Content-Type": "application/json"}
 
 
+@app.route("/sync-test")
+def sync_test():
+    # If the data test is not enabled, return 500
+    if STORAGE_PATH is None:
+        return {"error": "Data test is not enabled"}, 500
+
+    request_time = time()
+
+    # Write the data to the file
+    with open(STORAGE_PATH, "a") as f:
+        f.write(f"Request at {request_time}\n")
+        f.close()
+
+    # Read the data from the others nodes in parallel
+    data = getNodes()
+
+    response_time = time() - request_time
+
+    # Get only the last line from the other nodes
+    response = {}
+    for node in data:
+        if "last_line" not in node:
+            response[node["hostname"]] = "No data"
+        elif node["last_line"] != "Request at " + str(request_time):
+            response[node["hostname"]] = "Out of sync"
+        else:
+            response[node["hostname"]] = "In sync"
+
+    response["response_time"] = response_time
+
+    return dump(response), 200, {"Content-Type": "application/json"}
+
+
 if __name__ == "__main__":
     print(f" * Using Kubernetes Namespace: {NAMESPACE}")
+    if STORAGE_PATH is not None:
+        print(f" * Using Storage Path: {STORAGE_PATH}")
+        prepareStorage()
     app.run(host=HOST, port=PORT)
